923ed85cf04949cdb692890b2ede01a293bcd70c
demonlord2002 <drdoom2003p@gmail.com>
2025-08-22 02:25:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -94,8 +94,8 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
         [InlineKeyboardButton("📝 Click Here to Register", url=REGISTER_LINK)],
         [
-            InlineKeyboardButton("🦅 Wнιѕтℓє Sqυα∂-🇧🅢🇹⚚ḋʶ", url="https://t.me/BeastPaiyan"),
-            InlineKeyboardButton("🪭 Wнιѕтℓє Sqυα∂-𝐃꯭𝝾꯭𝗿𝝰꯭𝝴꯭֟፝𝗺꯭𝝾꯭𝝶꯭", url="https://t.me/Rubesh_official_18")
+            InlineKeyboardButton("🦅 Wнιѕтℓє Sqυα∂-BST", url="https://t.me/BeastPaiyan"),
+            InlineKeyboardButton("🪭 Wнιѕтℓє Sqυα∂-TVK", url="https://t.me/Rubesh_official_18")
         ]
     ]
 
