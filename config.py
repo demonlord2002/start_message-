e9a82661355043da0c039bd13e932006a5d1961c
demonlord2002 <drdoom2003p@gmail.com>
2025-08-21 21:46:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # ---------------- Bot Info ----------------
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token_here")
-BOT_OWNER_ID = int(os.getenv("BOT_OWNER_ID", "123456789"))  # Your Telegram ID
-BOT_DEVELOPER = os.getenv("BOT_DEVELOPER", "YourName")
-BOT_LINK = os.getenv("BOT_LINK", "https://t.me/yourusername")
-REGISTER_LINK = os.getenv("REGISTER_LINK", "https://t.me/your_registration_link")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8365258721:AAG47_D-GLCKMoiakwf0ONUFMVlRck-jwmw")
+BOT_OWNER_ID = int(os.getenv("BOT_OWNER_ID", "7049074888"))  # Your Telegram ID
+BOT_DEVELOPER = os.getenv("BOT_DEVELOPER", "𝙈𝙖𝙧𝙬𝙞𝙣")
+BOT_LINK = os.getenv("BOT_LINK", "https://t.me/SunsetOfMe")
+REGISTER_LINK = os.getenv("REGISTER_LINK", "https://docs.google.com/forms/d/e/1FAIpQLSfGyaA74UQdjftO-Qje70odt0HCkW4Bs9FLftpjBO1EQZ92OA/viewform?usp=dialog")
 
 # ---------------- MongoDB ----------------
-MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017/")
+MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://rubesh08virat:rubesh08virat@cluster0.d33p1rm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
